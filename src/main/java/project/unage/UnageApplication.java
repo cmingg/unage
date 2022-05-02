3ab554f03f6b6134bce915777fa015f42198b80f
cmingg <chooo12343@gmail.com>
2022-05-02 22:22:11 +0900

    Initial commit

---
+++ src/main/java/project/unage/UnageApplication.java
@@ -0,0 +1,13 @@
+package project.unage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/unage/UnageApplicationTests.java
@@ -0,0 +1,13 @@
+package project.unage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
