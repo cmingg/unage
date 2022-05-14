036ec4b9e95d9f7ceed28f77a8e0e409186884c8
cmingg <chooo12343@gmail.com>
2022-05-14 23:36:37 +0900

    아이디 찾기 - 모달창 구현중

--- src/main/java/com/project/unage/controller/LoginController.java
+++ src/main/java/com/project/unage/controller/LoginController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -21,6 +22,11 @@ public class LoginController {
 
     @GetMapping("/member/findId")
         public String findIdForm() {
+        return "/member/findId";
+    }
+
+    @PostMapping("/member/findId")
+    public String findId(@RequestParam("email") String email) {
         return "/member/findId";
     }
 
